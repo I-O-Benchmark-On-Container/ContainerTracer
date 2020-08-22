a8cea8a735aaace5bb32b6348c48df3ff16a971e
BlaCkinkGJ <ss5kijun@gmail.com>
2020-08-22 13:26:43 +0900

    [Runner] GPLv2 라이센스를 소스 코드에 추가했습니다. (#39)
    copyright로 GPLv2 라이센스를 소스 코드에 전부 추가했습니다.

--- include/log.h
+++ include/log.h
@@ -1,3 +1,27 @@
+/**
+ * @copyright "Container Tracer" which executes the container performance mesurements
+ * Copyright (C) 2020 BlaCkinkGJ
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @file log.h
+ * @brief 로그 출력 양식에 해당합니다.
+ * @author BlaCkinkGJ (ss5kijun@gmail.com)
+ * @version 0.1
+ * @date 2020-08-05
+ */
 #ifndef _LOG_H
 #define _LOG_H
 #include <time.h>
@@ -15,9 +39,6 @@
  * - `LOG_ERROR`: ERROR에 해당하는 로그만 출력합니다.
  * - `UNDEFINED`: 아무것도 출력하지 않습니다.
  */
-// #define LOG_INFO
-//#define LOG_WARNING
-//#define LOG_ERROR
 
 #if defined(LOG_INFO)
 #define pr_info(level, msg, ...)                                               \
