96ada1e1d83234d0ff857c83434f4af3d9a81041
BlaCkinkGJ <ss5kijun@gmail.com>
2020-08-22 04:06:56 +0000

    GPL 라이센스 부분을 삽입했습니다.

--- runner/driver/tr-driver/tr-info.c
+++ runner/driver/tr-driver/tr-info.c
@@ -1,4 +1,21 @@
 /**
+ * @copyright "Container Tracer" which executes the container performance mesurements
+ * Copyright (C) 2020 BlaCkinkGJ
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  * @file tr-info.c
  * @brief info 구조체를 초기화하는 역할을 합니다.
  * @author BlaCkinkGJ (ss5kijun@gmail.com)
