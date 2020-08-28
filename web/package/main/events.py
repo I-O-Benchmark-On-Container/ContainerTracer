2052815c08f61d29e690a57138250d386b16c04c
jsy0404 <pnmsegwyd@naver.com>
2020-08-28 07:54:14 +0000

    Exception handling with data_path, driver_path, device, weights

--- web/package/main/events.py
+++ web/package/main/events.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from flask import session
 from flask_socketio import emit
 from .. import socketio
@@ -31,11 +32,15 @@ def set_options(set_each, set_all):
     set_config.store(set_all, "set_all")
     set_config.store(set_each, "set_each")
     json_config = set_config.get_config_data()
-    # Must run set_config() before running run_all_trace_replay()
-    # container_tracer_factory = set_config.container_tracer_factory
-    # container_tracer = container_tracer_factory.get_instance(json_config)
-    container_tracer = set_config.set_container_tracer(json_config)
-    container_tracer.run_all_container_tracer()  # 여기서 trace-replay가 시작합니다
+    try:
+        container_tracer = set_config.set_container_tracer(json_config)
+        container_tracer.run_all_container_tracer()  # 여기서 trace-replay가 시작합니다
+    except OSError as e:
+        print(e, file=sys.stderr)
+        sys.exit(-1)
+    except Exception as e:
+        emit("Invalid", e)
+        return
 
     nr_cgroup = len(set_config.data["setting"]["task_option"])
     emit("chart_start", nr_cgroup)
